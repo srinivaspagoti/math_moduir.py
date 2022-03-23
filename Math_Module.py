df16ba8c0fa2cbf4400b9490e64b5fb8c81ae0e5
p srinivas <100501528+srinivaspagoti@users.noreply.github.com>
2022-03-23 07:32:54 +0530

    Add files via upload

---
+++ Math_Module.py
@@ -0,0 +1,84 @@
+# import math
+# print(math.ceil(0.1)) 
+# print(math.ceil(2.4))
+# print(math.ceil(5.3))
+# print(math.ceil(-5.3)) 
+# print(math.ceil(22.6))
+# print(math.ceil(10.0)) 
+# print(math.exp(1))
+# print(math.exp(3))
+# print(math.factorial(0)) 
+# print(math.factorial(1)) 
+# print(math.factorial(2)) 
+# print(math.factorial(3)) 
+# print(math.factorial(4)) 
+# print(math.factorial(5)) 
+# print(math.floor(23.4))
+# print(math.floor(23.5))
+# print(math.floor(23.6))
+# print(math.floor(-23.6))
+# print(math.floor(-19.0))
+# print(math.floor(-19.1))
+# print(math.ceil(75.0))
+# print(math.ceil(75.3))
+# print(math.floor(75.3))
+# print(math.ceil(75.5))
+
+''' when used math module we get answer
+    in terms of float'''
+# a=math.pow(3,2)
+# b=3**2
+# print(a,type(a))
+# print(b,type(b))
+# print(math.pi)
+# print(math.pi*2)
+# print(math.pi*3**2)
+# print(math.sqrt(4))
+# print(math.sqrt(6.7))
+# print(math.sqrt(24))
+# print(math.sqrt(25))
+# c=2   # 1 to 3 will give 1 if used isqrt
+# d=6   # 4 to 8 will give 2 if used isqrt
+# e=14   # 9 to 15 will give 3 if used isqrt
+# f=0   # 0 will give 0 if used isqrt
+# print(math.isqrt(c))
+# print(math.isqrt(d))
+# print(math.isqrt(e))
+# print(math.isqrt(f))
+
+
+# n=[1,2,3,4,5]
+# print(sum(n))
+# print(math.fsum(n))
+# print(math.prod(n))
+# print(int(math.fsum(n)))
+# g=15%3
+# print(g)
+# print(math.fmod(15,3))
+# v=-4
+# print(abs(v))
+# print(math.fabs(v))
+# print(math.cos(45))
+# print(math.sin(45))
+# print(math.tan(45))
+# print(math.trunc(4.55))
+# print(math.trunc(-6.55))
+# print(math.gcd(18,11))
+# print(math.gcd(18,13))
+# print(math.gcd(18,17))
+# print(math.gcd(18,19))
+# print(math.gcd(18,23))
+# print(math.gcd(18,29))
+# print(math.gcd(18,31))
+# print(math.gcd(18,37))
+# print(math.gcd(18,41))
+# print(math.gcd(18,43))
+# print(math.gcd(18,47))
+# print(math.gcd(24,32))
+# print(math.gcd(52,86))
+# print(math.gcd(6,16))
+# print(math.lcm(6,16))
+# print(math.lcm(8,14))
+
+import math
+print(dir(math))
